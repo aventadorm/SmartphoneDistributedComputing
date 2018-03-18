ab1af03ed903d27ce0a25d5cdee357a918febc55
aventadorm <mayur.phadte@gmail.com>
2018-03-17 21:04:25 -0400

    MD5 Hashing

--- app/src/main/java/com/mayurphadte/smartphonedistributedcomputing/ProcessingService.java
+++ app/src/main/java/com/mayurphadte/smartphonedistributedcomputing/ProcessingService.java
@@ -4,6 +4,9 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by mayurphadte on 13/03/18.
  */
@@ -15,12 +18,27 @@ public class ProcessingService extends IntentService {
     }
     @Override
     protected void onHandleIntent(Intent intent){
-        try{
-            Thread.sleep(5000);
-            Log.d("Hi", "hisii");
+        String string_to_be_converted_to_MD5 = "REPLACE_WITH YOUR_OWN_STRING";
+        String MD5_Hash_String = md5(string_to_be_converted_to_MD5);
+        Log.d("md5:", MD5_Hash_String);
+        Log.d("md5:", "Hi");
+    }
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
 
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+
+            return hexString.toString();
+        }catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
+        return "";
     }
 }
